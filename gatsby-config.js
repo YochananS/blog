ddec4854173f3b775c843d527884d79a0adf0bd7
YochananS <morehyochanan@gmail.com>
2020-08-04 11:56:45 +0300

    new blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: `Catalyst`,
-    description: `Speed up your GatsbyJS development workflow. Designed as a set of opinionated and advanced themes and starters using MDX and Theme-UI. Incorporates gatsby-theme-catalyst-core, gatsby-theme-catalyst-header-top, and gatsby-theme-catalyst-footer.`,
-    keywords: [`gatsby`, `theme`, `react`],
-    author: `Eric Howey`,
-    siteUrl: `https://gatsby-starter-catalyst.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    title: `Wisdom`,
+    description: `Wisdom not of the earth`,
+    keywords: [`wisdom`, `Judah`, `Spiritual food`],
+    author: `Servant`,
+    siteUrl: `https://wisdomofjudah.com`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
     menuLinks: [
+      
       {
-        name: `Page 1`,
-        link: `/page-1`,
-        type: `internal`, //internal or anchor
-      },
-      {
-        name: `Anchor 1`,
-        link: `#anchor-1`,
-        type: `anchor`, //internal or anchor
-      },
-      {
-        name: `Page 2`,
-        link: `/page-2`,
+        name: `Blog`,
+        link: `/blog`,
         type: `internal`, //internal or anchor
         subMenu: [
+          
           {
-            name: `Sub 1`,
-            link: `/sub-1`,
-            type: `internal`, //internal or anchor
-          },
-          {
-            name: `Sub 2`,
-            link: `/sub-2`,
+            name: `Lion of Judah Wisdom`,
+            link: `/lionofJudah`,
             type: `internal`, //internal or anchor
           },
         ],
@@ -37,19 +24,11 @@ module.exports = {
     socialLinks: [
       {
         name: `Email`,
-        link: `eric@erichowey.dev`,
+        link: `fake@email.com`,
         location: `footer`, //Options are "all", "header", "footer"
       },
-      {
-        name: `Twitter`,
-        link: `https://twitter.com/erchwy`,
-        location: `header`, //Options are "all", "header", "footer"
-      },
-      {
-        name: `Github`,
-        link: `https://www.github.com/ehowey`,
-        location: `all`, //Options are "all", "header", "footer"
-      },
+      
+  
     ],
   },
   plugins: [
@@ -82,7 +61,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#cccccc`,
         display: `minimal-ui`,
-        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+        icon: `content/assets/lion.jpg`, // This path is relative to the root of the site.
       },
     },
   ],
